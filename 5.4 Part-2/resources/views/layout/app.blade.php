94a8ad9ef10b9cb4294be37004ab16653b331a3b
Nuruzzaman Khan Nabwab <mr.nuruzzamancse@gmail.com>
2017-03-30 08:40:10 +0600

    Data,Blade&Layout

--- 5.4 Part-2/resources/views/layout/app.blade.php
+++ 5.4 Part-2/resources/views/layout/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Laravel @yield('title')</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,13 +79,8 @@
 
     <div class="content">
         <div class="title m-b-md">
-
-            @unless($bitfumes)
-                There is no data
-            @endunless
-            @foreach ($bitfumes as $bitfume)
-            {{$bitfume}}
-            @endforeach
+            @section('body')
+             @show
         </div>
 
         <div class="links">
--- 5.4 Part-2/routes/web.php
+++ 5.4 Part-2/routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome'); //welcome.blade.php
 });
 
+Route::get('about','PagesController@about');
+
 Route::get('about',function(){
 
     $bitfumes = [];
